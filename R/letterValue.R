eaeeea314366cb79e4fbf08bcaae467ceb5beee2
Tingting Zhan <tingtingzhan@gmail.com>
2025-04-21 11:48:51 -0400

    tzh

--- R/letterValue.R
+++ R/letterValue.R
@@ -11,13 +11,15 @@
 #' @param x \link[base]{double} \link[base]{vector}, observations \eqn{x},
 #' missingness ***not*** allowed.
 #' 
-#' @param g_ \link[base]{double} \link[base]{vector}, probabilities used for estimating \eqn{g} parameter.
-#' Or, use `g_ = FALSE` to implement the constraint \eqn{g=0} 
+#' @param probs_g \link[base]{double} \link[base]{vector}, probabilities used for estimating \eqn{g} parameter.
+#' Or, use `probs_g = FALSE` to implement the constraint \eqn{g=0} 
 #' (i.e., an \eqn{h}-distribution is estimated). 
 #' 
-#' @param h_ \link[base]{double} \link[base]{vector}, probabilities used for estimating \eqn{h} parameter.
-#' Or, use `h_ = FALSE` to implement the constraint \eqn{h=0}
+#' @param probs_h \link[base]{double} \link[base]{vector}, probabilities used for estimating \eqn{h} parameter.
+#' Or, use `probs_h = FALSE` to implement the constraint \eqn{h=0}
 #' (i.e., a \eqn{g}-distribution is estimated). 
+#' 
+#' @param g_select ..
 #' 
 #' @param ... additional parameters, currently not in use
 #' 
@@ -40,14 +42,13 @@
 #' thus extends `fitdistrplus::fitdist` for estimating Tukey \eqn{g}-&-\eqn{h} distributions.
 #' 
 #' @note
-#' Parameter `g_` and `h_` does not have to be truly unique; i.e., \link[base]{all.equal} elements are allowed.
+#' Parameter `probs_g` and `probs_h` does not have to be truly unique; i.e., \link[base]{all.equal} elements are allowed.
 #' 
 #' @returns 
 #' 
-#' Function [letterValue()] returns a `'letterValue'` object, 
-#' which is \link[base]{double} \link[base]{vector} of estimates \eqn{(\hat{A}, \hat{B}, \hat{g}, \hat{h})}
-#' for a Tukey \eqn{g}-&-\eqn{h} distribution.
-#' 
+#' Functions [letterValue_g()], [letterValue_h()] and [letterValue_gh()] 
+#' returns a \link[base]{length}-`4L` \link[base]{double} \link[base]{vector} of
+#' estimates \eqn{(\hat{A}, \hat{B}, \hat{g}, \hat{h})}.
 #' 
 #' @references 
 #' Hoaglin, D.C. (1985). Summarizing Shape Numerically: The \eqn{g}-and-\eqn{h} Distributions. 
@@ -56,12 +57,69 @@
 #' @keywords internal
 #' @name letterValue
 #' @import patchwork
-#' @importFrom stats mad median.default quantile
+#' @importFrom stats median.default
 #' @export
-letterValue <- function(
+letterValue_g <- function(
+    x,
+    probs_g = seq.int(from = .01, to = .3, by = .005),
+    g_select = 'median', # 'median' is good enough
+    ...
+) {
+  
+  if (anyNA(x)) stop('do not allow NA in observations')
+  A <- median.default(x)
+  x <- x - A # new median = 0
+  
+  probs_g <- probs_g |> check_letterVal_(x = x)
+  
+  g <- lv_g_(x, probs_g = probs_g, g_select = g_select) 
+  
+  B <- lv_B_(x, g = g, probs_g = probs_g)
+  
+  ret <- c(A = A, B = B, g = g, h = 0)
+  attr(ret, which = 'plot') <- 
+    attr(g, which = 'plot', exact = TRUE) +
+    attr(B, which = 'plot', exact = TRUE)
+  return(ret)
+
+}
+
+
+
+#' @rdname letterValue
+#' @import patchwork
+#' @importFrom stats median.default
+#' @export
+letterValue_h <- function(
+    x,
+    probs_h = seq.int(from = .1, to = .3, by = .005),
+    ...
+) {
+  
+  if (anyNA(x)) stop('do not allow NA in observations')
+  A <- median.default(x)
+  x <- x - A # new median = 0
+  
+  probs_h <- probs_h |> check_letterVal_(x = x)
+  
+  Bh <- lv_Bh_(x, g = 0, probs_h = probs_h)
+  
+  ret <- c(A = A, B = unname(Bh['B']), g = 0, h = unname(Bh['h']))
+  attr(ret, which = 'plot') <- attr(Bh, which = 'plot', exact = TRUE)
+  return(ret)
+  
+}
+
+
+#' @rdname letterValue
+#' @import patchwork
+#' @importFrom stats median.default
+#' @export
+letterValue_gh <- function(
   x,
-  g_ = seq.int(from = .01, to = .3, by = .005),
-  h_ = seq.int(from = .1, to = .3, by = .005),
+  probs_g = seq.int(from = .01, to = .3, by = .005),
+  probs_h = seq.int(from = .1, to = .3, by = .005),
+  g_select = 'h_optim',
   ...
 ) {
   
@@ -69,68 +127,58 @@ letterValue <- function(
   A <- median.default(x)
   x <- x - A # new median = 0
   
-  if (!isFALSE(g_)) {
-    if (!is.double(g_) || anyNA(g_)) stop('g_ (for estimating g) must be double without missing')
-    g_ <- g_ |> unique.default() |> sort.int()
-    if (!length(g_)) stop('`g_` cannot be len-0')
-    if (any(g_ <= 0, g_ >= .5)) stop('g_ (for estimating g) must be between 0 and .5 (not including)')
-    gL <- 0 - quantile(x, probs = g_) # lower-half-spread (LHS), the paragraph under eq.10 on p469
-    gU <- quantile(x, probs = 1 - g_) - 0 # upper-half-spread (UHS)
-    gok <- (gL != 0) & (gU != 0) # may ==0 due to small sample size
-    if (!any(gok)) stop('data degeneration')
-    if (!all(gok)) {
-      g_ <- g_[gok]
-      gL <- gL[gok]
-      gU <- gU[gok]
-    } # else do nothing
-  }
-
-  if (!isFALSE(h_)) {
-    if (!is.double(h_) || anyNA(h_)) stop('h_ (for estimating h) must be double without missing')
-    h_ <- h_ |> unique.default() |> sort.int()
-    if (!length(h_)) stop('`h_` cannot be len-0')
-    if (any(h_ <= 0, h_ >= .5)) stop('h_ (for estimating h) must be between 0 and .5 (not including)')
-    hL <- 0 - quantile(x, probs = h_) # lower-half-spread (LHS), the paragraph under eq.10 on p469
-    hU <- quantile(x, probs = 1 - h_) - 0 # upper-half-spread (UHS)
-    hok <- (hL != 0) & (hU != 0) # may ==0 due to small sample size
-    if (!any(hok)) stop('data degeneration')
-    if (!all(hok)) {
-      h_ <- h_[hok] 
-      hL <- hL[hok]
-      hU <- hU[hok]
-    } # else do nothing
+  probs_g <- probs_g |> check_letterVal_(x = x)
+  probs_h <- probs_h |> check_letterVal_(x = x)
+  
+  g <- lv_g_(x, probs_g = probs_g, probs_h = probs_h, g_select = g_select, ...)
+  if (g_select == 'demo') {
+    demo_plot <- mapply(FUN = \(g, nm, pane) {
+      names(g) <- nm
+      p <- lv_Bh_(x, g = g, probs_h = probs_h) |> 
+        attr(which = 'plot', exact = TRUE)
+      p + labs(title = sprintf(fmt = '(%s). %s', pane, nm))
+    }, g = g, nm = names(g), pane = LETTERS[seq_along(g)], SIMPLIFY = FALSE) |>
+      Reduce(f = `+`)
+    demo_plot <- demo_plot + 
+      (attr(g, which = 'plot', exact = 'TRUE') + 
+         labs(title = sprintf(fmt = '(%s). Estimated g', LETTERS[length(g) + 1L])))
+    return(demo_plot)
   }
   
-  g <- if (isFALSE(g_)) 0 else lv_g_(x, g_ = g_, gL = gL, gU = gU)
+  Bh <- lv_Bh_(x, g = g, probs_h = probs_h)
   
-  if (isFALSE(h_)) {
-    
-    if (g == 0) {
-      return(c(A = A, B = mad(x, center = 0), g = 0, h = 0))
-    }
-    
-    B <- lv_B_(x, g = g, g_ = g_)
-    ret <- c(A = A, B = B, g = g, h = 0)
-    attr(ret, which = 'plot') <- 
-      attr(g, which = 'plot', exact = TRUE) +
-      attr(B, which = 'plot', exact = TRUE)
-    return(ret)
-    
-  } 
-    
-  Bh <- lv_Bh_(x, g = g, h_ = h_, hL = hL, hU = hU) 
-  ret <- c(A = A, B = unname(Bh['B']), g = g, h = unname(Bh['h']))
-  
-  attr(ret, which = 'plot') <- if (g == 0) {
+  ret <- c(A = A, B = unname(Bh['B']), g = unname(g), h = unname(Bh['h']))
+  attr(ret, which = 'plot') <- attr(g, which = 'plot', exact = TRUE) +
     attr(Bh, which = 'plot', exact = TRUE)
-  } else {
-    attr(g, which = 'plot', exact = TRUE) +
-    attr(Bh, which = 'plot', exact = TRUE)
-  }
-  
   return(ret)
   
 }
+
+
+
+#' @importFrom stats quantile
+check_letterVal_ <- function(x, probs) {
+  if (!is.double(probs) || anyNA(probs)) stop('`probs` must be double without missing')
+  probs <- probs |> unique.default() |> sort.int()
+  if (!length(probs)) stop('`probs` cannot be len-0')
+  if (any(probs <= 0, probs >= .5)) stop('`probs` must be between 0 and .5 (not including)')
+  lhs <- - quantile(x, probs = probs) # lower-half-spread (LHS), the paragraph under eq.10 on p469
+  uhs <- quantile(x, probs = 1 - probs) # upper-half-spread (UHS)
+  gok <- (lhs != 0) & (uhs != 0) # may ==0 due to small sample size
+  if (!any(gok)) stop('data degeneration')
+  probs <- probs[gok]
+  attr(probs, which = 'lhs') <- lhs[gok]
+  attr(probs, which = 'uhs') <- uhs[gok]
+  return(probs)
+}
+
+
+
+
+
+
+
+
 
 
 
@@ -139,43 +187,38 @@ letterValue <- function(
 #' @importFrom stats .lm.fit qnorm quantile
 #' @importFrom scales label_percent
 #' @importFrom ggplot2 xlim
-lv_Bh_ <- function(x, g, h_, hL, hU) {
+lv_Bh_ <- function(
+    x, 
+    g, 
+    probs_h,
+    do.plot = TRUE,
+    ...
+) {
   
-  z <- qnorm(h_)
+  lhs <- attr(probs_h, which = 'lhs', exact = TRUE)
+  uhs <- attr(probs_h, which = 'uhs', exact = TRUE)
+  z <- qnorm(probs_h)
   regx <- z^2 / 2
   
   if (g == 0) {
     
-    regy <- log((hU+hL) / (-2*z)) # p483-eq(28); all.equal(hU+hL, q[-id] - q[id])
-    regyL <- log(-hL/z) # easy to derive from p483-eq(26a)
-    regyU <- log(-hU/z) # easy to derive from p483-eq(26b)
-    
-    subtitle <- c( # double check the code vs. label!
-      'Combined: $log(-(x_{1-p} - x_p)/(2z_p))',
-      'Lower: $log(x_p/z_p)', 
-      'Upper: $log(-x_{1-p})/z_p)'
-    )
-    
+    regy <- log((uhs+lhs) / (-2*z)) # p483-eq(28); all.equal(uhs+lhs, q[-id] - q[id])
+    regyL <- log(-lhs/z) # easy to derive from p483-eq(26a)
+    regyU <- log(-uhs/z) # easy to derive from p483-eq(26b)
+  
   } else {
     
-    regyU <- log(g * hU / expm1(-g*z)) # p487-eq(33)
-    regyL <- log(g * hL / (-expm1(g*z))) # see the equation on bottom of p486
+    regyU <- log(g * uhs / expm1(-g*z)) # p487-eq(33)
+    regyL <- log(g * lhs / (-expm1(g*z))) # see the equation on bottom of p486
     
     # p487, paragraph under eq(33)
     regy1 <- (regyU + regyL) / 2 # 'averaging the logrithmic results'
-    regy2 <- log(g * (hU+hL) / (exp(-g*z) - exp(g*z))) # 'dividing the full spread with appropriate denominator', 
+    regy2 <- log(g * (uhs+lhs) / (exp(-g*z) - exp(g*z))) # 'dividing the full spread with appropriate denominator', 
     # `regy2` is obtained by summing up of the numeritor and denominator of the equation on bottom of p486
     # (expm1(x) - expm1(y)) == (exp(x) - exp(y))
     # I prefer `regy2`
     regy <- regy2
     
-    subtitle <- c( # double check the code vs. label!
-      'Lower: $log(-g(-x_p)/(exp(gz_p) - 1))',
-      'Upper: $log(g(x_{1-p})/(exp(-gz_p) - 1))',
-      # 'Combo v1: averaging response of Lower & Upper',
-      # 'Combo v2: $log(g(x_{1-p} - x_p)/(exp(-gz_p)-exp(gz)))'
-      'Combo: $log(g(x_{1-p} - x_p)/(exp(-gz_p)-exp(gz)))'
-    )
   }
   
   X <- cbind(1, regx)
@@ -185,21 +228,18 @@ lv_Bh_ <- function(x, g, h_, hL, hU) {
   
   # c('both', 'lower', 'upper')
   B <- exp(c(cf[1L], cfL[1L], cfU[1L]))
-  h <- pmax(0, c(cf[2L], cfL[2L], cfU[2L]))
+  h <- c(cf[2L], cfL[2L], cfU[2L])
   # slope `h` should be positive. When a negative slope is fitted, use h = 0
 
-  ret <- c(B = B[1L], h = h[1L])
-  
-  #nms <- sprintf(fmt = '%s\nB\u0302=%.3f, h\u0302=%.3f', c(
-  #  ### 'Combo (average log)', 
-  #  ### 'Combo (full-spread)', 
-  #  'Combo',
-  #  'Lower', 'Upper'), Bh$B, Bh$h)
+  ret <- c(B = B[1L], h = pmax(0, h[1L]))
+  attr(ret, which = 'errB') <- (cfL[1L] - cfU[1L])^2
+  attr(ret, which = 'errh') <- (cfL[2L] - cfU[2L])^2
+  if (!do.plot) return(ret) # when doing `g`-selection
   
   hs <- 1:3 |> # c('both', 'lower', 'upper')
     as.character() |>
-    rep(each = length(h_))
-  est <- sprintf(fmt = 'B=%.3f\nh=%.3f', B, h)
+    rep(each = length(probs_h))
+  est <- sprintf(fmt = '%s\nB=%.3f\nh=%.3f', c('Both', 'Lower', 'Upper'), B, h)
   mp <- aes(
     x = rep(regx, times = 3L), 
     y = c(regy, regyL, regyU), 
@@ -221,12 +261,12 @@ lv_Bh_ <- function(x, g, h_, hL, hU) {
     labs(
       y = NULL,
       caption = if (g == 0) {
-        'Given g = 0\nCombined: p483, eq(27)\nUpper & Lower: p483, eq(26a-26b)'
-      } else sprintf(fmt = 'Given g = %.3f\nUpper: p487,eq(33)\nLower: see p486,bottom', g)
+        'Constrained at g = 0'
+      } else sprintf(fmt = '%s g = %.3f', names(g), g)
     ) + 
     theme(
       legend.position = 'inside',
-      legend.position.inside = c(.8, .2),
+      legend.position.inside = c(.8, .25),
       legend.key.spacing.y = unit(.01, units = 'npc')
     )
   
@@ -249,11 +289,11 @@ lv_Bh_ <- function(x, g, h_, hL, hU) {
 #' @importFrom latex2exp TeX
 #' @importFrom scales pal_hue
 #' @importFrom stats .lm.fit qnorm quantile
-lv_B_ <- function(x, g, g_) {
+lv_B_ <- function(x, g, probs_g) {
   # when (h == 0) && (g != 0), calculate B (p469, Example, p471-eq(11a))
-  q <- quantile(x, probs = c(g_, 1-g_)) - 0
-  z <- qnorm(g_) # length n
-  id <- seq_along(g_)
+  q <- quantile(x, probs = c(probs_g, 1-probs_g)) - 0
+  z <- qnorm(probs_g) # length n
+  id <- seq_along(probs_g)
   regx <- expm1(c(g*z, -g*z))/g # p469, eq(8a-8b)
   B <- unname(.lm.fit(x = cbind(regx), y = cbind(q))$coefficients)
   if (B < 0) stop('estimated B < 0')
@@ -263,7 +303,7 @@ lv_B_ <- function(x, g, g_) {
   if (BU < 0) stop('estimated B (upper spread) < 0')
   
   hs <- c('lower', 'upper') |>
-    rep(each = length(g_))
+    rep(each = length(probs_g))
   mp <- aes(x = regx, y = q, color = hs, fill = hs)
     
   attr(B, which = 'plot') <- ggplot() + 
@@ -284,12 +324,12 @@ lv_B_ <- function(x, g, g_) {
       x = c(mean(regx[id]), mean(regx[-id]), mean(regx)), y = c(mean(q[id]), mean(q[-id]), mean(q)), 
       colour = c(pal_hue()(2L), 'grey40'),
       fontface = c('plain', 'plain', 'bold'),
-      #label = sprintf(fmt = '$\\hat{B} = %.3f$', c(BL, BU, B)) |> TeX() # why warning??
+      #label = sprintf(fmt = 'Slope$\\hat{B} = %.3f$', c(BL, BU, B)) |> TeX() # why warning??
       label = sprintf(fmt = 'Slope\nB=%.3f', c(BL, BU, B))
     ) +
     labs(
       y = '$t_p$' |> TeX(), 
-      caption = sprintf(fmt = 'Given g = %.3f; h = 0\nLower Half Spread: p469, eq(8a)\nUpper Half Spread: p469, eq(8b)', g)
+      caption = sprintf(fmt = '%s g = %.3f; h = 0', names(g), g)
     )
   
   return(B)
@@ -301,19 +341,59 @@ lv_B_ <- function(x, g, g_) {
 
 
 
-#' @importFrom stats qnorm quantile
+#' @importFrom stats optimize qnorm quantile
 #' @importFrom ggplot2 ggplot aes geom_point geom_hline scale_x_continuous scale_y_continuous dup_axis guide_axis labs
 #' @importFrom latex2exp TeX
 #' @importFrom scales label_percent
-lv_g_ <- function(x, g_, gL, gU) {
+lv_g_ <- function(
+    x, 
+    probs_g, 
+    probs_h,
+    g_select = c('median', 'B_optim', 'h_optim', 'demo'),
+    ...
+) {
+  
+  lhs <- attr(probs_g, which = 'lhs', exact = TRUE)
+  uhs <- attr(probs_g, which = 'uhs', exact = TRUE)
   # p469, equation (10) Estimating g; must use both half-spread
-  gs <- (log(gL / gU) / qnorm(g_)) # p469, equation (10)
-  g <- median.default(gs) 
-  # p474, 2nd paragraph, the values of $g_p$ do not show regular dependence on $g_$,
-  # thus we take their median as a constant-$g$ description of the skewness.
+  gs <- (log(lhs / uhs) / qnorm(probs_g)) # p469, equation (10)
+  
+  g_q4 <- quantile(gs, probs = c(.25, .5, .75))
+  
+  g_select <- match.arg(g_select)
+  
+  if (g_select %in% c('median', 'demo')) {
+    g_median_ <- unname(g_q4[2L])
+    # p474, 2nd paragraph, the values of $g_p$ do not show regular dependence on $probs_g$,
+    # thus we take their median as a constant-$g$ description of the skewness.
+  }
+  
+  if (g_select %in% c('B_optim', 'demo')) {
+    g_B_ <- optimize(f = \(g) {
+      lv_Bh_(x = x, g = g, probs_h = probs_h, do.plot = FALSE) |>
+        attr(which = 'errB', exact = TRUE)
+    }, lower = g_q4[1L], upper = g_q4[3L])$minimum
+  }
+  
+  if (g_select %in% c('h_optim', 'demo')) {
+    g_h_ <- optimize(f = \(g) {
+      lv_Bh_(x = x, g = g, probs_h = probs_h, do.plot = FALSE) |>
+        attr(which = 'errh', exact = TRUE)
+    }, lower = g_q4[1L], upper = g_q4[3L])$minimum
+  }
+  
+  g <- switch(g_select, median = {
+    c(median = g_median_)
+  }, B_optim = {
+    c(B_optim = g_B_)
+  }, h_optim = {
+    c(h_optim = g_h_)
+  }, demo = {
+    c(median = g_median_, B_optim = g_B_, h_optim = g_h_)
+  })
   
   attr(g, which = 'plot') <- ggplot() + 
-    geom_point(mapping = aes(x = g_, y = gs)) + 
+    geom_point(mapping = aes(x = probs_g, y = gs)) + 
     geom_hline(yintercept = g, linetype = 2L, alpha = .2) +
     scale_x_continuous(name = 'p', labels = label_percent()) +
     scale_y_continuous(
@@ -321,11 +401,10 @@ lv_g_ <- function(x, g_, gL, gU) {
       sec.axis = dup_axis(
         name = NULL, 
         breaks = g, 
-        labels = \(i) sprintf(fmt = 'median\n%.3f', i),
-        guide = guide_axis(angle = 90)
+        labels = sprintf(fmt = '%s\n%.3f', names(g), g),
+        guide = guide_axis(angle = switch(g_select, demo = 45, 90))
       )
-    ) +
-    labs(caption = 'p469, eq(10)')
+    )
   
   return(g)
 }
